a9e556c6fda2932aa3999a2eed3eaac391069bd0
Jordan Salas <jordansalas447@gmail.com>
2019-06-27 01:28:10 -0500

    Merge

---
+++ CDatos/Manager/DepositoManager.cs
@@ -0,0 +1,51 @@
+﻿using Modelos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDatos.Manager
+{
+    public class DepositoManager
+    {
+        public bool Insert(Deposito Dep)
+        {
+            
+                using (var connection = Util.ConnectionFactory.conexion())
+                {
+                    connection.Open();
+
+                    SqlTransaction sqlTran = connection.BeginTransaction();
+
+                    SqlCommand command = connection.CreateCommand();
+
+                    command.Transaction = sqlTran;
+
+                    command.Parameters.AddWithValue("@NroCuenta", 12345);
+                    command.Parameters.AddWithValue("@Monto", 1000);
+                    command.Parameters.AddWithValue("@doi", 12345);
+
+
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "DepositosInsert";
+
+                     command.ExecuteNonQuery();
+
+                    // Commit the transaction.
+                    sqlTran.Commit();
+
+                    connection.Close();
+                    connection.Dispose();
+
+                    //if (afectados > 0)
+                        return true;
+                    //else
+                    //    return false;
+                }
+           
+        }
+    }
+}
