b961b29cc18b989ea2a8037863f9d533b74d83b1
ExperimentalLife <allamocca@xbusiness.dev>
2019-06-29 15:59:57 -0500

    cambios de hambre

--- GUI/Login.cs
+++ GUI/Login.cs
@@ -11,39 +11,60 @@ using System.Windows.Forms;
 namespace Sistema_Bancario
 {
 
-    public partial class Login : Form
-    {
-        public Login()
-        {
-            InitializeComponent();
-        }
-        //Variables globales
-        public Modelos.Modelos.PersonaModel usuarioLogin;
-        public Modelos.Modelos.SucursalModel SucursalUsuario;
+   public partial class Login : Form
+   {
+      private string m_username = null;
+      private string m_password = null;
+      public Login()
+      {
+         InitializeComponent();
+      }
+      //Variables globales
+      public Modelos.Modelos.PersonaModel usuarioLogin;
+      public Modelos.Modelos.SucursalModel SucursalUsuario;
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string nick = txtUsuario.Text;
-            string pass = txtPass.Text;
-            using (WsSistemaBancario.PersonaServiceClient user= new WsSistemaBancario.PersonaServiceClient())
+      private void SetLogin()
+      {
+         try
+         {
+
+            if (!string.IsNullOrEmpty(this.txtUsuario.Text.Trim()))
+            { this.m_username = this.txtUsuario.Text.Trim(); }
+            else
+            { this.m_username = null; }
+
+            if (!string.IsNullOrEmpty(this.txtPass.Text.Trim()))
+            { this.m_password = this.txtPass.Text.Trim(); }
+            else
+            { this.m_password = null; }
+         }
+         catch (Exception)
+         { return; }
+      }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+         SetLogin();
+
+         using (WsSistemaBancario.PersonaServiceClient user = new WsSistemaBancario.PersonaServiceClient())
+         {
+            usuarioLogin = user.Persona_ValidarUsuario(this.m_username, this.m_password);
+
+            if (usuarioLogin != null)
             {
-                usuarioLogin = user.Persona_ValidarUsuario(nick, pass);
-
-                if (usuarioLogin != null)
-                {
-                    VentanaPrincipal x = new VentanaPrincipal();
-                    SucursalUsuario = user.Persona_ObtenerSucursal(usuarioLogin.Id);
-                    this.LimpiarControles();
-                    this.Hide();
-                }
-                else MessageBox.Show("Credenciales incorrectas");
-                //else MessageBox.Show("error");
+               VentanaPrincipal x = new VentanaPrincipal();
+               SucursalUsuario = user.Persona_ObtenerSucursal(usuarioLogin.Id);
+               this.LimpiarControles();
+               this.Hide();
             }
-        }
-        private void LimpiarControles()
-        {
-            this.txtUsuario.Text = "";
-            this.txtPass.Text = "";
-        }
-    }
+            else MessageBox.Show("Credenciales incorrectas");
+            //else MessageBox.Show("error");
+         }
+      }
+      private void LimpiarControles()
+      {
+         this.txtUsuario.Text = "";
+         this.txtPass.Text = "";
+      }
+   }
 }
