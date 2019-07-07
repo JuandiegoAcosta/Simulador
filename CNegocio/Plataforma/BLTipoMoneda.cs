fa0c54b1f5499ce3f758336400a1cfccd67f9144
Jeancarlo Flores <J.FLORES.ES@hotmail.com>
2019-07-07 17:37:59 -0500

    Plataforma9

--- CNegocio/Plataforma/BLTipoMoneda.cs
+++ CNegocio/Plataforma/BLTipoMoneda.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CDatos.Manager;
+using Modelos.Modelos;
 
 namespace CNegocio.Plataforma
 {
     public class BLTipoMoneda
     {
+        private TipoMonedaManager ADTipoMonedaManager = new TipoMonedaManager();
 
+        public bool Insert(TipoMonedaModel aTipoMoneda)
+        {
+            return ADTipoMonedaManager.Insert(aTipoMoneda);
+        }
 
+        public bool Update(TipoMonedaModel aTipoMoneda)
+        {
+            return ADTipoMonedaManager.Update(aTipoMoneda);
+        }
+
+        public bool Delete(int aID_Moneda)
+        {
+            return ADTipoMonedaManager.Delete(aID_Moneda);
+        }
+
+        public TipoMonedaModel GetTipoMonedaModel(int aID_Moneda)
+        {
+            return ADTipoMonedaManager.GetTipoMonedaModel(aID_Moneda);
+        }
+
+        public List<TipoMonedaModel> TipoMonedaModelSelectAll()
+        {
+            return ADTipoMonedaManager.TipoMonedaModelSelectAll();
+        }
 
     }
 }
