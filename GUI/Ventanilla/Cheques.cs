9f63aed75a508dedff9bffd81528e2f6b16c8a95
Visual Studio <vstudio@isur.edu.pe>
2019-07-08 09:56:58 -0500

    Agregado cobro de cheques

---
+++ GUI/Ventanilla/Cheques.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Bancario.Ventanilla
+{
+    public partial class Cheques : Base
+    {
+        public Cheques()
+        {
+            InitializeComponent();
+            nroCuenta1.Visible = false;
+        }
+        private static Cheques _instance;
+        public static Cheques instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Cheques();
+                }
+
+                return _instance;
+            }
+        }
+
+        private void CKDeposito_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CKDeposito.Checked)
+            {
+                nroCuenta1.Visible = true;
+            }
+            else
+            {
+                nroCuenta1.Visible = false;
+            }
+            
+        }
+    }
+}
--- GUI/Ventanilla/Tarjetas.Designer.cs
+++ GUI/Ventanilla/Tarjetas.Designer.cs
@@ -31,6 +31,9 @@
             this.nroTarjeta1 = new Sistema_Bancario.Controles.NroTarjeta();
             this.monto1 = new Sistema_Bancario.Monto();
             this.tipoMoneda1 = new Sistema_Bancario.Controles.TipoMoneda();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // proceder1
@@ -48,7 +51,7 @@
             // 
             // monto1
             // 
-            this.monto1.Location = new System.Drawing.Point(26, 87);
+            this.monto1.Location = new System.Drawing.Point(26, 249);
             this.monto1.Margin = new System.Windows.Forms.Padding(2);
             this.monto1.Name = "monto1";
             this.monto1.Size = new System.Drawing.Size(298, 25);
@@ -56,15 +59,35 @@
             // 
             // tipoMoneda1
             // 
-            this.tipoMoneda1.Location = new System.Drawing.Point(26, 53);
+            this.tipoMoneda1.Location = new System.Drawing.Point(26, 215);
             this.tipoMoneda1.Margin = new System.Windows.Forms.Padding(2);
             this.tipoMoneda1.Name = "tipoMoneda1";
-            this.tipoMoneda1.Size = new System.Drawing.Size(233, 24);
+            this.tipoMoneda1.Size = new System.Drawing.Size(260, 24);
             this.tipoMoneda1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(621, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Tarjetas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.tipoMoneda1);
             this.Controls.Add(this.monto1);
             this.Controls.Add(this.nroTarjeta1);
@@ -76,6 +99,9 @@
             this.Controls.SetChildIndex(this.nroTarjeta1, 0);
             this.Controls.SetChildIndex(this.monto1, 0);
             this.Controls.SetChildIndex(this.tipoMoneda1, 0);
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -85,5 +111,7 @@
         private Controles.NroTarjeta nroTarjeta1;
         private Monto monto1;
         private Controles.TipoMoneda tipoMoneda1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
